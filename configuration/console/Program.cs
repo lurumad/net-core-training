c878875b78eabde32bf4f2ecea8c53834848d68a
Luis Ruiz Pavón <lruiz@plainconcepts.com>
2017-03-31 11:07:07 +0200

    Add more examples & labs

---
+++ configuration/console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
